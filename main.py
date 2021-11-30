04bf4eb8e7d382029ede111b5c31b886940dd1c6
agoniko <59967831+agoniko@users.noreply.github.com>
2021-11-30 21:35:18 +0100

    changed training with autokeras

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from training import get_model
 from testing import test
 
 if __name__ == '__main__':
-    #model =get_model()
-    testing = test()
+    model =get_model()
+    #testing = test()
--- training.py
+++ training.py
@@ -1,3 +1,5 @@
+import pandas as pd
+from numpy import asarray
 from tensorflow.keras.optimizers import RMSprop
 from keras.preprocessing.image import ImageDataGenerator
 import cv2
@@ -10,47 +12,59 @@ from sklearn.metrics import f1_score
 from sklearn.utils import shuffle
 import imutils
 import numpy as np
+import autokeras as ak
+from tqdm import tqdm
+from PIL import Image
+import glob
+from sklearn.utils import shuffle
 
 
+def create_df():
+    """
+    Creates a df (with shuffled entries)
+    with two columns: file name and label (0: normal or 1: pneumonia)
+    """
+    path = 'dataset'
+    files1 = glob.glob(path+"/with_mask/*.jpeg")
+    files1.extend(glob.glob(path+"/with_mask/*.jpg"))
+    files2 = glob.glob(path+"/without_mask/*.jpeg")
+    files2.extend(glob.glob(path + "/without_mask/*.jpg"))
+    # creating the train set
+    df_n = pd.DataFrame()
+    df_p = pd.DataFrame()
+    df_n['name'] = [x for x in files1]
+    df_n['outcome'] = 0.
+    df_p['name'] = [x for x in files2]
+    df_p['outcome'] = 1.
+    df = pd.concat([df_n, df_p], axis=0, ignore_index=True)
+    df = shuffle(df)  # shuffle the dataset
 
+    return df
+
+def create_x_and_y(train_df):
+    X = np.array([img_preprocess(p) for p in train_df.name.values])
+    y = train_df.outcome.values
+
+    return X, y
+
+
+def img_preprocess(img):
+    """
+    Opens the image and does some preprocessing
+    such as converting to RGB, resize and converting to array
+    """
+    img = Image.open(img)
+    img = img.convert('RGB')
+    img = img.resize((256, 256))
+    img = asarray(img) / 255
+    return img
 
 def get_model():
-    model = Sequential([
-        Conv2D(100, (3, 3), activation='relu', input_shape=(150, 150, 3)),
-        MaxPooling2D(2, 2),
+   X,y = create_x_and_y(create_df())
+   print(len(X))
+   print(len(y))
+   model = ak.ImageClassifier(max_trials = 50, metrics='accuracy')
+   model.fit(X, y, epochs=3, validation_split=0.2)
+   model = model.export_model()
+   model.save('model.h5')
 
-        Conv2D(100, (3, 3), activation='relu'),
-        MaxPooling2D(2, 2),
-
-        Flatten(),
-        Dropout(0.5),
-        Dense(50, activation='relu'),
-        Dense(2, activation='softmax')
-    ])
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-
-    TRAINING_DIR = "./train"
-    train_datagen = ImageDataGenerator(rescale=1.0 / 255,
-                                       rotation_range=40,
-                                       width_shift_range=0.2,
-                                       height_shift_range=0.2,
-                                       shear_range=0.2,
-                                       zoom_range=0.2,
-                                       horizontal_flip=True,
-                                       fill_mode='nearest')
-    train_generator = train_datagen.flow_from_directory(TRAINING_DIR,
-                                                        batch_size=10,
-                                                        target_size=(150, 150))
-    VALIDATION_DIR = "./test"
-    validation_datagen = ImageDataGenerator(rescale=1.0 / 255)
-    validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,
-                                                                  batch_size=10,
-                                                                  target_size=(150, 150))
-
-    checkpoint = ModelCheckpoint('model2-{epoch:03d}.model', monitor='val_loss', verbose=0, save_best_only=True,
-                                 mode='auto')
-
-    history = model.fit(train_generator,
-                                  epochs=10,
-                                  validation_data=validation_generator,
-                                  callbacks=[checkpoint])
